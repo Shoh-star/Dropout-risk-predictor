6ce61451e076de37d2f0f8718b232c4f6604f9bd
Shoh-star <howwillirememberit@gmail.com>
2025-10-12 14:27:08 +0300

    Update ML_StudentDropout.py

--- ML_StudentDropout.py
+++ ML_StudentDropout.py
@@ -16,14 +16,14 @@ X = df.drop(["Mother's occupation", "Father's occupation", "Mother's qualificati
             'Daytime/evening attendance\t', 'Age at enrollment', 'Displaced', 'Curricular units 2nd sem (enrolled)', 'Curricular units 1st sem (enrolled)', 'Previous qualification', 'Previous qualification (grade)', 'Debtor', 'Curricular units 2nd sem (credited)', 'Curricular units 1st sem (credited)', 'Gender', 'Nacionality', 'International', 'Educational special needs'], axis=1)
 y = (df['Target'] == 'Dropout').astype(int)
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.5, random_state=42)
+    X, y, test_size=0.4, random_state=42)
 smote = SMOTE(random_state=42)
 X_train_oversample, y_train_oversample = smote.fit_resample(X_train, y_train)
 X_train_scaled = scaler.fit_transform(
     X_train_oversample)
 X_test_scaled = scaler.transform(X_test)
-model = RandomForestClassifier(n_estimators=79, max_depth=10,
-                               class_weight='balanced', min_samples_split=4, max_leaf_nodes=15, random_state=42)
+model = RandomForestClassifier(n_estimators=69, max_depth=8,
+                               class_weight='balanced', min_samples_leaf=5, min_samples_split=7, max_leaf_nodes=15, random_state=42)
 # model = KNeighborsClassifier(n_neighbors=5)
 # model = BernoulliNB()
 # model = LogisticRegression(class_weight='balanced', max_iter=1000)
@@ -40,4 +40,3 @@ print(classification_report(y_train_oversample, prediction2))
 joblib.dump(model, "dropout_model.pkl")
 feature_columns = X.columns.tolist()
 joblib.dump(feature_columns, "model_features.pkl")
-
