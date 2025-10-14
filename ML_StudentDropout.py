ec0abad2a5e60e3f348b3750e9e6afcdc3f57453
Shoh-star <howwillirememberit@gmail.com>
2025-10-14 21:03:08 +0300

    Update ML_StudentDropout.py

--- ML_StudentDropout.py
+++ ML_StudentDropout.py
@@ -11,35 +11,39 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, classification_report, r2_score, confusion_matrix
 df = pd.read_csv('Dropout_Student.csv', sep=';')
 
-scaler = StandardScaler()
+# scaler = StandardScaler()
 # List of columns to drop if they exist
 optional_cols = ['Application mode', 'Email',
-    "Mother's occupation", "Father's occupation", "Mother's qualification", "Father's qualification",
-    'Student_ID', 'Full_Name', 'Target', 'Marital status', 'Daytime/evening attendance\t',
-    'Age at enrollment', 'Displaced', 'Curricular units 2nd sem (enrolled)',
-    'Curricular units 1st sem (enrolled)', 'Previous qualification', 'Previous qualification (grade)',
-    'Debtor', 'Curricular units 2nd sem (credited)', 'Curricular units 1st sem (credited)',
-    'Gender', 'Nacionality', 'International', 'Educational special needs'
-]
+                 "Mother's occupation", "Father's occupation", "Mother's qualification", "Father's qualification",
+                 'Student_ID', 'Full_Name', 'Target', 'Marital status', 'Daytime/evening attendance\t',
+                 'Age at enrollment', 'Displaced', 'Curricular units 2nd sem (enrolled)',
+                 'Curricular units 1st sem (enrolled)', 'Previous qualification', 'Previous qualification (grade)',
+                 'Debtor', 'Curricular units 2nd sem (credited)', 'Curricular units 1st sem (credited)',
+                 'Gender', 'Nacionality', 'International', 'Educational special needs'
+                 ]
+
+# Filter only the columns that exist in the DataFrame
 cols_to_drop = [col for col in optional_cols if col in df.columns]
+
+# Drop safely
 X = df.drop(cols_to_drop, axis=1)
 y = (df['Target'] == 'Dropout').astype(int)
+
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.4, random_state=42)
 smote = SMOTE(random_state=42)
 X_train_oversample, y_train_oversample = smote.fit_resample(X_train, y_train)
-X_train_scaled = scaler.fit_transform(
-    X_train_oversample)
-X_test_scaled = scaler.transform(X_test)
+# X_train_scaled = scaler.fit_transform(X_train_oversample)
+# X_test_scaled = scaler.transform(X_test)
 model = RandomForestClassifier(n_estimators=69, max_depth=8,
                                class_weight='balanced', min_samples_leaf=5, min_samples_split=7, max_leaf_nodes=15, random_state=42)
 # model = KNeighborsClassifier(n_neighbors=5)
 # model = BernoulliNB()
 # model = LogisticRegression(class_weight='balanced', max_iter=1000)
-model.fit(X_train_scaled, y_train_oversample)
+model.fit(X_train_oversample, y_train_oversample)
 # columns = pd.DataFrame({'Feature': X.columns})
-prediction = model.predict(X_test_scaled)
-prediction2 = model.predict(X_train_scaled)
+prediction = model.predict(X_test)
+prediction2 = model.predict(X_train_oversample)
 # print('Test Accuracy: ', accuracy_score(y_test, prediction))
 # print('Train Accuracy: ', accuracy_score(y_train_oversample, prediction2))
 print(classification_report(y_test, prediction))
@@ -49,6 +53,3 @@ print(classification_report(y_train_oversample, prediction2))
 joblib.dump(model, "dropout_model.pkl")
 feature_columns = X.columns.tolist()
 joblib.dump(feature_columns, "model_features.pkl")
-
-
-
